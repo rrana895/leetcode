d1827102f26b8c63e6f3682ef30caff4e9b13c34
rrana895 <rachirana84@gmail.com>
2022-06-04 18:52:30 -0400

    Added SQL Day5

---
+++ SQL_StudyPlan/Day5/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+
+
+select customer_id,count(visit_id) as count_no_trans
+from Visits 
+where visit_id not in (select visit_id from Transactions)
+group by customer_id
+order by count_no_trans desc
